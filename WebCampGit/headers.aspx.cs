1602d1d06fa10188df4bd10c8cc71d48d4d16229
Puneet Gupta <puneetg@microsoft.com>
2021-12-21 12:34:38 +0530

    Printing Request-Id Header

---
+++ WebCampGit/headers.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demomvp
+{
+    public partial class headers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Headers["Request-Id"] != null)
+            {
+                lblMessage.Text = Request.Headers["Request-Id"].ToString();
+                return;
+            }
+
+            lblMessage.Text = "Header Request-Id not found";
+        }
+    }
+}
